65894982e22643ca37ccd77fea82df4d9cd2a8d9
Luis Enrique Ortiz Ramirez <luisortramirez2211@gmail.com>
2020-02-16 19:31:53 -0600

    Added smoke test

--- hola/tests.py
+++ hola/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
+from django.contrib.auth.models import User
+from django.core.exceptions import ValidationError
 
-# Create your tests here.
+
+class TestHumo(TestCase):
+
+    def test_prueba_humo(self):
+        self.assertEqual(2 + 2, 4)
